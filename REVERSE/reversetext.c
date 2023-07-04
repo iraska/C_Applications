29f87f6aca43564d94402294fa9a407853acbff5
İrem Turna <siremturna@gmail.com>
2023-07-04 17:18:33 +0300

    Proje dosyası ekle.

---
+++ REVERSE/reversetext.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n = 0, j = 0;
+    char a[100], b[100];
+
+    printf("\nEnter the String:");
+    gets(a);
+
+    // String lenght
+    for (i = 0; a[i] != '\0'; i++)
+    {
+        n++;
+    }
+
+    for (i = 0; a[i] != '\0'; i++)
+    {
+        b[j] = a[n - i - 1];
+        j++;
+    }
+
+    b[j] = '\0';
+    printf("\nReversed String:%s", b);
+
+    return 0;
+}
