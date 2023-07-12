90b5815e22c95a0ad208faf07274e041c7d9740a
İrem Turna <siremturna@gmail.com>
2023-07-12 19:40:16 +0300

    fgets.c created

---
+++ REVERSE/fgets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[100];
+
+    printf("Enter a word or phrase: ");
+    fgets(input, sizeof(input), stdin);
+
+    // Remove unwanted '\n' character
+    if (input[strlen(input) - 1] == '\n')
+        input[strlen(input) - 1] = '\0';
+
+    int lenght = strlen(input);
+    printf("Length of the entered word/phrase: %d\n", lenght);
+
+    return 0;
+}
